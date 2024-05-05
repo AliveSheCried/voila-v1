19bcbd4ab6e96867cca0a2f850454de2f187da44
AliveSheCried <robert@hood.gb.net>
2024-05-05 21:37:13 +0100

    Update styles for merchant account inputs and add alignment class for buttons

--- client/src/components/MerchantAccounts/GetPayoutDetail/GetPayoutDetail.jsx
+++ client/src/components/MerchantAccounts/GetPayoutDetail/GetPayoutDetail.jsx
@@ -1,42 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import PayoutSearch from "./PayoutSearch";
 
 const GetPayoutDetail = () => {
-  const [transactions, setTransactions] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/api/transactions")
-      .then((response) => setTransactions(response.data.transactions))
-      .catch((error) => setError(error));
-  }, []);
-
-  console.log(transactions);
-  if (error) return <div>Error loading transactions.</div>;
   return (
     <div>
       <div className="content__head">
-        <span className="content__arrow">&raquo;</span> Payout detail
+        <span className="content__arrow">&raquo;</span> Get payout detail
       </div>
-      <ul>
-        {transactions.map((tx) => (
-          <li key={tx._id}>
-            {tx.account_holder_name} - {tx.amount_in_minor / 100}
-          </li>
-        ))}
-      </ul>
+
+      <PayoutSearch />
     </div>
   );
-  //   return (
-  //     <div>
-  //       <div className="content__head">
-  //         <span className="content__arrow">&raquo;</span> Get payout detail
-  //       </div>
-
-  //       <PayoutSearch />
-  //     </div>
-  //   );
 };
 
 export default GetPayoutDetail;
--- client/src/components/MerchantAccounts/GetPayoutDetail/PayoutSearch.jsx
+++ client/src/components/MerchantAccounts/GetPayoutDetail/PayoutSearch.jsx
@@ -1,6 +1,43 @@
 //import PropTypes from "prop-types";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PayoutSearch = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [error, setError] = useState(null);
+  const [suggestions, setSuggestions] = useState([]);
+  const [input, setInput] = useState("");
+  const [search, setSearch] = useState({
+    amount: "",
+    currency: "",
+    createdDate: "",
+    payoutId: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/transactions")
+      .then((response) => setTransactions(response.data.transactions))
+      .catch((error) => setError(error));
+  }, []);
+
+  // console.log("error", error);
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInput(value);
+    if (value.length > 0) {
+      const filtered = transactions.filter((tx) =>
+        tx.account_holder_name.toLowerCase().startsWith(value.toLowerCase())
+      );
+      setSuggestions(filtered);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  if (error) return <div>Something went wrong. D'oh!</div>;
+
   return (
     <div className="token__container">
       <div className="token__title">
@@ -13,66 +50,119 @@ const PayoutSearch = () => {
       </div>
       <div className="merchant-account__search-container">
         <div>
-          <div className="content__label">Disbursement account</div>
-          <select>
-            <option value="">-- Select account --</option>
-          </select>
-        </div>
-        <div>
           <div className="content__label">Payee</div>
-          <div className="input__merchant-account merchant-account__search-dates text-sm">
+          <div className="input__merchant-account text-sm">
             <div>
               <div>
-                <input type="text" id="payeeName" />
+                <input
+                  className="input-payee-name"
+                  type="text"
+                  id="payeeName"
+                  value={input}
+                  onChange={handleInputChange}
+                  placeholder="Type account holder name"
+                />
               </div>
             </div>
           </div>
         </div>
         <div>
-          <div className="content__label">Date range - from & to</div>
-          <div className="input__merchant-account merchant-account__search-dates text-sm">
+          <div className="content__label">Currency</div>
+          <div className="input__merchant-account text-sm">
             <div>
               <div>
-                <input type="date" id="dateFrom" />
-              </div>
-            </div>
-            <div>
-              <div>
-                <input type="date" id="dateTo" />
+                <input
+                  className="input-currency"
+                  type="text"
+                  id="currency"
+                  value={search.currency}
+                  readOnly={true}
+                />
               </div>
             </div>
           </div>
-          <div className="absolute-right sp-top-sm">
-            <button
-              className="btn btn--tertiary"
-              // className={`btn ${
-              //   !selectedIban || !dateFrom || !dateTo
-              //     ? "btn--tertiary-inactive"
-              //     : "btn--tertiary"
-              // }`}
-              onClick={() => {
-                console.log("Search transactions");
-              }}
-            >
-              Search transactions
-            </button>
+        </div>
+        <div>
+          <div className="content__label">Payout amount</div>
+          <div className="input__merchant-account text-sm">
+            <div>
+              <div>
+                <input
+                  type="text"
+                  id="payoutAmount"
+                  value={(search.amount / 100).toFixed(2)}
+                  readOnly={true}
+                  placeholder="Payout amount"
+                />
+              </div>
+            </div>
           </div>
+        </div>
+        <div>
+          <div className="content__label">Created date</div>
+          <div className="input__merchant-account text-sm">
+            <div>
+              <div>
+                <input
+                  className="input-created-date"
+                  type="text"
+                  id="createdDate"
+                  value={
+                    search.createdDate
+                      ? new Date(search.createdDate).toISOString().split("T")[0]
+                      : ""
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="btn__align-bottom">
+          <button
+            className={`btn ${
+              !search.payoutId ? "btn--tertiary-inactive" : "btn--tertiary"
+            }`}
+            onClick={() => {
+              console.log("Search transactions");
+            }}
+          >
+            Search transactions
+          </button>
         </div>
       </div>
+      <div className="merchant-account__search-container">
+        <ul className={`text-sm ${suggestions.length != 0 ? "test1" : ""}`}>
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion._id}
+              onClick={() => {
+                setInput(suggestion.account_holder_name);
+                setSearch((prevSearch) => ({
+                  ...prevSearch,
+                  currency: suggestion.currency,
+                  createdDate: suggestion.created_at,
+                  amount: suggestion.amount_in_minor,
+                  payoutId: suggestion.payoutId,
+                }));
+                setSuggestions([]);
+              }}
+            >
+              <span className="test2">{suggestion.account_holder_name}</span>
+              <span className="test3">{suggestion.currency}</span>
+              <span className="test4">
+                {(suggestion.amount_in_minor / 100).toFixed(2)}
+              </span>
+
+              {suggestion.created_at
+                ? new Date(suggestion.created_at).toISOString().split("T")[0]
+                : ""}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="merchant-account__search-container"></div>
     </div>
   );
 };
-
-// PayoutSearch.propTypes = {
-//   onAccountChange: PropTypes.func.isRequired,
-//   onDateFromChange: PropTypes.func.isRequired,
-//   onDateToChange: PropTypes.func.isRequired,
-//   onGetTransactions: PropTypes.func.isRequired,
-//   dateFrom: PropTypes.string.isRequired,
-//   dateTo: PropTypes.string.isRequired,
-//   //selectedAccountId: PropTypes.string.isRequired,
-//   selectedIban: PropTypes.string.isRequired,
-//   merchantAccounts: PropTypes.array.isRequired,
-// };
 
 export default PayoutSearch;
